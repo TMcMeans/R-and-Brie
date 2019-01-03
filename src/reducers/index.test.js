c8e97976fb777a9e544b66f801541b82815fc1ad
Tanjie McMeans <tanjmcmeans01@gmail.com>
2019-01-03 11:01:46 -0700

    Add smoke tests to rootReducer tests for initial state of Redux state tree

---
+++ src/reducers/index.test.js
@@ -0,0 +1,18 @@
+import rootReducer from './index';
+import { createStore } from 'redux';
+import { errorReducer } from './errorReducer';
+import { loaderReducer } from './loaderReducer';
+import { recipeReducer } from './recipeReducer';
+
+describe('rootReducer', () => {
+  let store = createStore(rootReducer);
+  it('should show initial state of error message when the errorReducer handles an action', () => {
+    expect(store.getState().error).toEqual(errorReducer('', {}));
+  });
+  it('should show initial state of isLoading when the loaderReducer handles an action', () => {
+    expect(store.getState().isLoading).toEqual(loaderReducer(false, {}));
+  });
+  it('should show initial state of recipes when the recipeReducer handles an action', () => {
+    expect(store.getState().recipes).toEqual(recipeReducer([], {}));
+  });
+});
