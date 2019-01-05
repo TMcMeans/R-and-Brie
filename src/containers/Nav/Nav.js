fed4b0fd02896bc0f9e3b03a8696d1874240a23b
Tanjie McMeans <tanjmcmeans01@gmail.com>
2019-01-05 15:05:30 -0700

    modify and style nav component

--- src/containers/Nav/Nav.js
+++ src/containers/Nav/Nav.js
@@ -13,33 +13,63 @@ class Nav extends Component {
     return (
       <div className="nav">
         <Link className="nav-item" to="/recipes">
+          <div className="cheese">
+            <img src="" alt="" id="brie" />
+          </div>
           Brie
         </Link>
         <Link className="nav-item" to="/recipes">
+          <div className="cheese">
+            <img src="" alt="" id="parmesean" />
+          </div>
           Parmesan
         </Link>
         <Link className="nav-item" to="/recipes">
+          <div className="cheese">
+            <img src="" alt="" id="feta" />
+          </div>
           Feta
         </Link>
         <Link className="nav-item" to="/recipes">
+          <div className="cheese">
+            <img src="" alt="" id="swiss" />
+          </div>
           Swiss
         </Link>
         <Link className="nav-item" to="/recipes">
-          Mozarella
+          <div className="cheese">
+            <img src="" alt="" id="mozzarella" />
+          </div>
+          Mozzarella
         </Link>
         <Link className="nav-item" to="/recipes">
+          <div className="cheese">
+            <img src="" alt="" id="cheddar" />
+          </div>
           Cheddar
         </Link>
         <Link className="nav-item" to="/recipes">
+          <div className="cheese">
+            <img src="" alt="" id="blue" />
+          </div>
           Blue
         </Link>
         <Link className="nav-item" to="/recipes">
+          <div className="cheese">
+            <img src="" alt="" id="asiago" />
+          </div>
           Asiago
         </Link>
         <Link className="nav-item" to="/recipes">
+          <div className="cheese">
+            <img src="" alt="" id="fontina" />
+          </div>
           Fontina
         </Link>
         <Link className="nav-item" to="/recipes">
+          <div className="cheese">
+            <img src="" alt="" id="american" />
+          </div>
           American
         </Link>
       </div>
