0ae38e947b023a0905db4d23db5d00b54b7b677f
Tanjie McMeans <tanjmcmeans01@gmail.com>
2019-01-05 17:08:07 -0700

    Add and style welcome component

---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Welcome = () => {
+  return (
+    <div className="welcome">
+      <h1>Welcome, fellow cheese lover!</h1>
+      <p>
+        This app is designed to help you discover and favorite recipes by
+        searching popular cheese types as the main ingredient. Select a cheese
+        type at the top to view related recipes.
+      </p>
+      <p>Enjoy, and bon apetit!</p>
+    </div>
+  );
+};
