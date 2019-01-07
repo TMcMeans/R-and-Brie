ec76030ad4ea0d9c341a15234344ea4ac072a2ab
Tanjie McMeans <tanjmcmeans01@gmail.com>
2019-01-07 16:49:30 -0700

    Add loader componenet and refactor reviewsReducer to save reviews to state

--- src/containers/ReviewForm/ReviewForm.js
+++ src/containers/ReviewForm/ReviewForm.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import StarRatingComponent from 'react-star-rating-component';
+import { connect } from 'react-redux'
+
+import { addReview } from '../../actions/index'
 
 class ReviewForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      review: '',
-      rating: 0
+      caption: '',
+      rating: 0,
+      label: ''
     };
   }
 
@@ -14,13 +18,29 @@ class ReviewForm extends Component {
     this.setState({ rating: nextValue });
   };
 
+  handleChange = (e) => {
+    const { name, value } = e.target
+    this.setState({
+      [name]: value,
+      label: this.props.label
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const { reviews } = this.props
+    if (!reviews.includes(this.state.label)) {
+      this.props.addReview(this.state)
+    }
+  }
+
   render() {
     const { rating } = this.state;
 
     return (
       <div className="review">
-        <form action="">
-          <input type="text" placeholder="Add a comment..." />
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="Add a comment..." name="caption" value={this.state.caption} onChange={this.handleChange} />
           <h2>Rating from state: {rating}</h2>
           <StarRatingComponent
             name="rate1"
@@ -30,11 +50,19 @@ class ReviewForm extends Component {
             starColor={`#ffb400`}
             emptyStarColor={`#000`}
           />
-          <button>Post</button>
+          <input type="submit" />
         </form>
       </div>
     );
   }
 }
 
-export default ReviewForm;
+const mapStateToProps = (state) => ({
+  reviews: state.reviews
+})
+
+const mapDispatchToProps = dispatch => ({
+  addReview: (review) => dispatch(addReview(review))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(ReviewForm);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import { errorReducer } from './errorReducer';
 import { loaderReducer } from './loaderReducer';
 import { recipeReducer } from './recipeReducer';
+import { reviewReducer } from './reviewReducer';
 
 const rootReducer = combineReducers({
   error: errorReducer,
   isLoading: loaderReducer,
-  recipes: recipeReducer
+  recipes: recipeReducer,
+  reviews: reviewReducer,
 });
 
 export default rootReducer;
