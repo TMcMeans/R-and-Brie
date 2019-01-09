1f8b3659d521ded2b2a3c425e0a99a0a72b6f770
Tanjie McMeans <tanjmcmeans01@gmail.com>
2019-01-08 20:09:10 -0700

    Add more styling

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -2,8 +2,25 @@ import React from 'react'
 
 export const Loader = () => {
   return (
-    <div className="loader">
-      <h1>Loading</h1>
+    <div class="container">
+      <div class="circle">
+        <div class="bread crust">
+          <div class="top-bread"></div>
+          <div class="bottom-bread"></div>
+          <div class="bread crumb">
+            <div class="top-bread"></div>
+            <div class="bottom-bread"></div>
+            <div class="spread">
+              <div class="pb">
+              </div>
+              <div class="jam">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="wait">
+        <h2 className="waiting">Just a sec...</h2></div>
     </div>
   )
 }
